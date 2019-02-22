9c707a4d14701a39018bd865e9f8955fa94d37a7
Cyberdog90 <43635561+Cyberdog90@users.noreply.github.com>
2019-02-23 01:47:48 +0900

    Update nearly_array.py

--- nearly_array.py
+++ nearly_array.py
@@ -8,21 +8,3 @@ def near(array, value):
             ret = i
 
     return ret
-
-#  main.py修正したらやる
-
-"""
-def near(array, value):
-    length = len(array)
-    ans = 11451419198109318933349800364364
-    ret = []
-    for i in range(length):
-        abans = abs(array[i] - int(value))
-        if abans < ans:
-            ans = abans
-            ret.append(array[i])
-            ret.append(i)
-
-    return ret
-
-"""
